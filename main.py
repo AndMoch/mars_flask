62af3ca2b0a2f010ca205899dd1a9f2fa03937be
AndMoch <andmochgeek@ya.ru>
2023-02-25 15:08:43 +0300

    choice/planet_name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
@@ -69,6 +69,40 @@ def promotion_image():
                       </div>
                       <div class="alert alert-danger" role="alert">
                         Присоединяйся!
+                      </div>
+                    </body>
+                  </html>"""
+
+
+@app.route("/choice/<planet_name>")
+def choice(planet_name):
+    return f"""<!doctype html>
+                  <html lang="en">
+                    <head>
+                      <meta charset="utf-8">
+                      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                      <link rel="stylesheet" 
+                      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                      integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                      crossorigin="anonymous">
+                      <title>Варианты выбора</title>
+                    </head>
+                    <body>
+                      <h1>Мое предложение: {planet_name}</h1>
+                      <div class="alert alert-light" role="alert">
+                        Эта планета близка к Земле;
+                      </div>
+                      <div class="alert alert-success" role="alert">
+                        На ней много необходимых ресурсов
+                      </div>
+                      <div class="alert alert-secondary" role="alert">
+                        На ней есть вода и атмосфера;
+                      </div>
+                      <div class="alert alert-warning" role="alert">
+                        На ней есть небольшое магнитное поле;
+                      </div>
+                      <div class="alert alert-danger" role="alert">
+                        Наконец, она просто красива!
                       </div>
                     </body>
                   </html>"""
