15393fb807031831ffec086e979e0443b54b8336
AndMoch <andmochgeek@ya.ru>
2023-03-07 18:53:49 +0300

    auto_answer

--- main.py
+++ main.py
@@ -199,5 +199,31 @@ def list_prof(list):
     return render_template("job_list.html", list=list)
 
 
+@app.route("/answer")
+def answer():
+    param = {'title': "Анкета",
+             'surname': "Watny",
+             'name': "Mark",
+             'education': "выше среднего",
+             'profession': "штурман марсохода",
+             'sex': "male",
+             'motivation': "Всегда мечтал застрять на Марсе!",
+             'ready': "True"}
+    return render_template("auto_answer.html", **param)
+
+
+@app.route("/auto_answer")
+def auto_answer():
+    param = {'title': "Анкета",
+             'surname': "Watny",
+             'name': "Mark",
+             'education': "выше среднего",
+             'profession': "штурман марсохода",
+             'sex': "male",
+             'motivation': "Всегда мечтал застрять на Марсе!",
+             'ready': "True"}
+    return render_template("auto_answer.html", **param)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
